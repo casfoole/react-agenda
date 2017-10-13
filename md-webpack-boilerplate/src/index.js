15216d055af09b4603bcd40bf3ca7b6a3a95395f
Cas Foole <22937@ma-web.nl>
2017-10-13 11:25:37 +0200

    boilerplate
    opdracht van het vak sct

---
+++ md-webpack-boilerplate/src/index.js
@@ -0,0 +1,8 @@
+import '../sass/main.scss';
+// window.history.pushState("object or string", "Title", "/binch");
+
+const multiply = (value, multiplier) => value * multiplier;
+
+var d = new Date();
+d.setTime(1332403882588);
+window.print("THICCCCCC");
